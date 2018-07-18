83e0d5ba9028dab9a25762012f3a0dd7b0f1b742
Philippe Bolduc <pbolduc2354@gmail.com>
2018-07-18 15:38:13 +0000

    exercice 1, 2, 3, 4 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.name = "Bello"
+@store1.save
+
+puts @store1.inspect
+puts @store2.inspect
